1a6d36ebc6ec79bfbbc7bd5b55aee72fd3d6a768
Serhii Klunniy <s.klunniy@gmail.com>
2024-02-17 21:55:48 +0200

    commit

--- src/main/java/ua/klunniy/spring/hibernate/models/hibernate1/Main.java
+++ src/main/java/ua/klunniy/spring/hibernate/models/hibernate1/Main.java
@@ -9,18 +9,22 @@ import org.hibernate.cfg.Configuration;
  */
 public class Main {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration()
-                .addAnnotatedClass(Item.class);
+        Configuration configuration = new Configuration().addAnnotatedClass(Item.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
         Item item = new Item("tv-test");
         try {
             session.beginTransaction();
-            session.save(item);
+
+            Item item1 = session.get(Item.class, 1);
+            System.out.println(item1.getName());
+
             session.getTransaction().commit();
+
         } finally {
             sessionFactory.close();
         }
     }
+
 }
