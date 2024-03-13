1d60053c559a1aaf669ac83738b70e8967896657
Serhii Klunniy <s.klunniy@gmail.com>
2024-03-13 06:30:06 -0400

    commit

--- src/main/java/ua/klunniy/spring/multithreading/first/MyClass2.java
+++ src/main/java/ua/klunniy/spring/multithreading/first/MyClass2.java
@@ -1,6 +1,6 @@
 package ua.klunniy.spring.multithreading.first;
 
-public class MyClass2 implements Runnable{
+public class MyClass2 implements Runnable {
     @Override
     public void run() {
         System.out.println("run");
